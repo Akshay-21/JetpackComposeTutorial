777308c6a84238107791b904d44df2b95b00e6c2
Akshay Kumar Maurya <akshay21sep@gmail.com>
2023-11-01 01:38:29 +0530

    Designed profile details

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -5,9 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
@@ -23,12 +32,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MessageCard(msg:Message) {
 
-    Row{
+    Row(modifier = Modifier.padding(8.dp)){
         AsyncImage(model = ("https://developer.android.com/static/images/jetpack/compose-tutorial/profile_picture.png"),
-            contentDescription = "Contact Profile Picture")
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                // Setting image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape))
 
+        // Added a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+        
         Column {
             Text(text = msg.author)
+
+            // Added a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
